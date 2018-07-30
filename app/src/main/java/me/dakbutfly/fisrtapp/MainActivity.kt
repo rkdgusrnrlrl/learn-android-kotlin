833860c65274bc91a1e21eda228deb70d4837083
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-07-30 22:25:15 +0900

    feat : text edit 에 쓴 글은 text view 에 띄우는 이벤트를 버튼에 바인딩함

--- app/src/main/java/me/dakbutfly/fisrtapp/MainActivity.kt
+++ app/src/main/java/me/dakbutfly/fisrtapp/MainActivity.kt
@@ -2,11 +2,16 @@ package me.dakbutfly.fisrtapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnChangeTxt.setOnClickListener {
+            var text = textEdit.text
+            textView.text = text
+        }
     }
 }
